f9538cd64c393a2321c4276d64340c21b644b66f
ghazyayman <muhghazyayman@gmail.com>
2022-09-24 00:57:52 +0800

    add phone number

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,56 @@ class _ProfileAppState extends State<ProfileApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(child: Column(
+        home: Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: SafeArea(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50.0,
-              backgroundImage: ,
+              backgroundImage: AssetImage('assets/img/me.jpg'),
+            ),
+            Text(
+              'Ghazy',
+              style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.blueAccent.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.blueAccent.shade100,
+              ),
             )
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueAccent,
+                  ),
+                  title: Text('+62 812 3456 7890',
+                      style: TextStyle(
+                          color: Colors.blueAccent.shade700,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0)),
+                ))
           ],
         ),
       ),
-    );
+    ));
   }
 }
